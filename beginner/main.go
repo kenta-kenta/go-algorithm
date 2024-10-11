2d6d27997ab04d45baace06724aa148bb13ed5d8
kenta-kenta <kenken22352235@gmail.com>
2024-10-11 13:42:26 +0900

    2, 3問目

--- beginner/main.go
+++ beginner/main.go
@@ -5,6 +5,13 @@ import (
 )
 
 func main() {
+	// FizzBuzz()
+	// Reverse("こんにちは")
+	list := []int{-40, 6, 9, 50}
+	fmt.Println(MinNumber(list))
+}
+
+func FizzBuzz() {
 	for i := 1; i <= 100; i++ {
 		switch {
 		case i%15 == 0:
@@ -17,4 +24,28 @@ func main() {
 			fmt.Println(i)
 		}
 	}
+}
+
+func Reverse(str string) {
+	runes := []rune(str)
+	end := len(runes)
+	n := end / 2
+	for i := 0; i < n; i++ {
+		runes[i], runes[end-i-1] = runes[end-i-1], runes[i]
+	}
+	fmt.Println(string(runes))
+}
+
+func MinNumber(list []int) (int, error) {
+	min := list[0]
+	if len(list) == 0 {
+		return 0, fmt.Errorf("配列が空です")
+	} else {
+		for _, num := range list {
+			if min > num {
+				min = num
+			}
+		}
+	}
+	return min, nil
 }
